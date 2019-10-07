a0518231b92ae0aa4d2560b6da48e1225fe7aa0a
PetejStone <petejstoney@gmail.com>
2019-10-07 15:38:20 -0700

    passed iterative binary stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,30 +11,66 @@ def linear_search(arr, target):
       print(i)
   return -1
     
-  #TO-DO: add missing code
 
+#def binary_search(to_find, names):
+    # Cut our list in half, examine the midpoint item
+    # start = 0
+    # end = len(names)
+    # while end - start > 0:
+    #     mid = start + (end - start) // 2
+    #     item = names[mid]
+    #     # If the item is equal to to_find:
+    #     if item == to_find:
+    #         return True
+    #     # Otherwise, if it's smaller
+    #     elif item > to_find:
+    #         end = mid - 1
+    #         # Repeat binary search on first half of the list
+    #     # Otherwise
+    #     else:
+    #         start = mid + 1
+    #         # Repeat binary search on second half
+    # return False
    
-
-
 # STRETCH: write an iterative implementation of Binary Search 
+
 def binary_search(arr, target):
-
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
-
-  return -1 # not found
-
-
+    #Cut our list in half, examine the midpoint item
+    start = 0
+    end = len(arr) -1
+    while start <= end:
+        # length of array // 2
+        mid = start + (end - 1) // 2 
+        item = arr[mid]
+        # If the item is equal to to_find:
+        if item == target:
+            return arr.index(item)
+        # Otherwise, if it's smaller
+        elif item > target:
+           # print(item)
+            end = mid - 1
+            # Repeat binary search on first half of the list
+        # Otherwise
+        elif item < target:
+            print(item)
+            start = mid + 1
+            # Repeat binary search on second half
+    return -1
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)//2
+  
 
   if len(arr) == 0:
     return -1 # array empty
+  
+  mid = (low+high)//2
+  item = arr[mid]
+
+  if item == target:
+    return item
+  elif item > target:
+    return binary_search_recursive(arr[:mid], target, low, high)
+  else:
+    return binary_search_recursive(arr[mid +1:], target, low, high)
   # TO-DO: add missing if/else statements, recursive calls
